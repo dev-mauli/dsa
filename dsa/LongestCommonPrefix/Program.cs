63391a4ff7cc7a328a3a068d6b2389aebd02f1ea
Pandurang Lad <premlad961@gmail.com>
2026-01-02 17:01:15 +0530

    Implemented LongestCommonPrefix

---
+++ dsa/LongestCommonPrefix/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+static string LongestCommonPrefix(string[] strs)
+{
+    StringBuilder result = new StringBuilder();
+
+    Array.Sort(strs);
+
+    char[] first = strs[0].ToCharArray();
+    char[] last = strs[strs.Length - 1].ToCharArray();
+
+    for (int i = 0; i < first.Length; i++)
+    {
+        if (first[i] == last[i])
+        {
+            result.Append(first[i]);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return result.ToString();
+}
+
+LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+
+Console.WriteLine("Test");
