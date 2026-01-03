d93e54c04eb47d8906bfbd0f39d5149316d16432
Pandurang Lad <premlad961@gmail.com>
2026-01-03 16:29:07 +0530

    Added MedianOfTwoSortedArrays

---
+++ dsa/MedianOfTwoSortedArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+{
+    int[] mergedArr = nums1.Concat(nums2).ToArray();
+    int left = 0;
+    int right = mergedArr.Length - 1;
+    while (left < right)
+    {
+        left++;
+        right--;
+    }
+    double median = (double)(mergedArr[left] + mergedArr[right]) / 2;
+    return median;
+}
+
+FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+
+Console.WriteLine("Test");
---
+++ dsa/ReverseArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void reverseArray(int[] arr)
+{
+    // code here
+    int left = 0;
+    int right = arr.Length - 1;
+    while (left < right)
+    {
+        (arr[left],arr[right]) = (arr[right], arr[left]);
+        left++;
+        right--;
+    }
+}
+
+reverseArray(new int[] { 12, 35, 1, 10, 34, 1 });
+
+Console.WriteLine("Test");
