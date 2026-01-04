30448b6a28b4c0c879e7fe16d51955465687d829
Pandurang Lad <premlad961@gmail.com>
2026-01-04 22:27:49 +0530

    RotateArr and 3Sum Problem Added

---
+++ dsa/RotateArr/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static void RotateArr(int[] arr, int d)
+{
+    // Code Here
+    int n = arr.Length;
+    d = d % n;
+
+    ReverseArr(arr, 0, d - 1);
+    ReverseArr(arr, d, n - 1);
+    ReverseArr(arr, 0, n - 1);
+}
+
+static void ReverseArr(int[] arr, int left, int right)
+{
+    while (left < right)
+    {
+        (arr[left], arr[right]) = (arr[right], arr[left]);
+        left++;
+        right--;
+    }
+}
+
+RotateArr(new int[] { 1, 2, 3, 4, 5 }, 2);
+
+Console.WriteLine("Test");
---
+++ dsa/ThreeSum/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+static IList<IList<int>> ThreeSum(int[] nums)
+{
+    if (nums.Length < 3)
+    {
+        return new List<IList<int>>();
+    }
+    Array.Sort(nums);
+    HashSet<IList<int>> result = new HashSet<IList<int>>();
+    for (int i = 0; i < nums.Length - 2; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int left = i + 1;
+        int right = nums.Length - 1;
+        while (left < right)
+        {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0)
+            {
+                result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++; right--;
+            }
+            else if (sum < 0)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    }
+    return result.ToList();
+}
+
+ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+
+Console.WriteLine("Test");
