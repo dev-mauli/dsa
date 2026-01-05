e091d675e71759a4e69dd3015bd543c210e351a9
Pandurang Lad <premlad961@gmail.com>
2026-01-05 12:34:12 +0530

    Added NextPermutation and RemoveDuplicatesFromSortedArray

---
+++ dsa/NextPermutation/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void nextPermutation(int[] arr)
+{
+    // code here
+    int n = arr.Length;
+    int pivot = -1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            pivot = i;
+            break;
+        }
+    }
+    if (pivot == -1)
+    {
+        Array.Reverse(arr);
+        return;
+    }
+    for (int i = n - 1; i > pivot; i--)
+    {
+        if (arr[i] > arr[pivot])
+        {
+            (arr[i], arr[pivot]) = (arr[pivot], arr[i]);
+            break;
+        }
+    }
+    int left = pivot + 1;
+    int right = n - 1;
+    while (left < right)
+    {
+        (arr[left], arr[right]) = (arr[right], arr[left]);
+        left++;
+        right--;
+    }
+}
+
+nextPermutation(new int[] { 2, 4, 1, 7, 5, 0 });
+
+Console.WriteLine("Test");
---
+++ dsa/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+static int RemoveDuplicates(int[] nums)
+{
+    int left = 1;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[left] = nums[i];
+            left++;
+        }
+    }
+    return left;
+}
+
+RemoveDuplicates(new int[] { 1, 1, 2, 2, 3, 4, 4, 5 });
+
+Console.WriteLine("Test");
