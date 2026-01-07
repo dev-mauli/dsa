c156668cf83d1828b3a7d1a3c5678d971115f27e
Pandurang Lad <premlad961@gmail.com>
2026-01-07 21:51:32 +0530

    Added StockBuySellMultipleTransactions

---
+++ dsa/StockBuySellMultipleTransactions/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int MaximumProfit(int[] prices)
+{
+    int maxProfit = 0;
+    for (int i = 1; i < prices.Length; i++)
+    {
+        if (prices[i] > prices[i - 1])
+        {
+            maxProfit = prices[i] - prices[i - 1];
+        }
+    }
+    return maxProfit;
+}
+
+MaximumProfit(new int[] { 100, 180, 260, 310, 40, 535, 695 });
+
+Console.WriteLine("Test");
