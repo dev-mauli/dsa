ee88e6ea0eeaaa53e6b9c10ba31d783414da387d
Pandurang Lad <premlad961@gmail.com>
2026-01-10 00:50:23 +0530

    Implemented ContainerWithMostWater and MinimizeTheHeights

---
+++ dsa/ContainerWithMostWater/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int MaxArea(int[] height)
+{
+    int left = 0;
+    int right = height.Length - 1;
+    int maxArea = 0;
+    while (left < right)
+    {
+        int width = right - left;
+        int minHeight = Math.Min(height[left], height[right]);
+        int area = width * minHeight;
+
+        maxArea = Math.Max(maxArea, area);
+
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+    return maxArea;
+}
+
+MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+
+Console.WriteLine("Test");
---
+++ dsa/MinimizeTheHeights/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int getMinDiff(int[] arr, int k)
+{
+    // code here
+    int result = 0;
+    int n = arr.Length;
+    Array.Sort(arr);
+    result = arr[n - 1] - arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] - k < 0) { continue; }
+        int min = Math.Min(arr[0] + k, arr[i] - k);
+        int max = Math.Max(arr[i - 1] + k, arr[n - 1] - k);
+        result = Math.Min(result, max - min);
+    }
+    return result;
+}
+
+getMinDiff(new int[] { 1, 5, 8, 10 }, 2);
+
+Console.WriteLine("Test");
