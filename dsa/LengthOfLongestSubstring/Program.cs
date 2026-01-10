d1370a95d35fee2ea82e1e676bbe42fc8e870b97
Pandurang Lad <premlad961@gmail.com>
2026-01-10 22:54:18 +0530

    Added LengthOfLongestSubstring and MaximumSumSubarray

---
+++ dsa/LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int LengthOfLongestSubstring(string s)
+{
+    int n = s.Length;
+    int maxLength = 0;
+    Dictionary<char, int> map = new Dictionary<char, int>();
+    for (int left = 0, right = 0; right < n; right++)
+    {
+        if (map.ContainsKey(s[right]))
+        {
+            left = Math.Max(map[s[right]] + 1, left);
+        }
+        map[s[right]] = right;
+        maxLength = Math.Max(maxLength, right - left + 1);
+    }
+    return maxLength;
+}
+
+LengthOfLongestSubstring("abcabcbb");
---
+++ dsa/MaximumSumSubarray/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int maxSubarraySum(int[] arr)
+{
+    // code here
+    int result = arr[0];
+    int maxEnding = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        maxEnding = Math.Max(arr[i], maxEnding + arr[i]);
+        result = Math.Max(result, maxEnding);
+    }
+    return result;
+}
+
+maxSubarraySum(new int[] { 2, 3, -8, 7, -1, 2, 3 });
+
+Console.WriteLine("Test");
