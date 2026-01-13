2b6ddb8a1069a45e011b9cbd4a418bc21d6ca98c
Pandurang Lad <premlad961@gmail.com>
2026-01-13 23:30:45 +0530

    Added AnagramCheck and FindIndexofFirstOccurence

---
+++ dsa/AnagramCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static bool areAnagrams(string s1, string s2)
+{
+    // code here
+    Dictionary<char, int> charCount = new Dictionary<char, int>();
+    foreach (char c in s1)
+    {
+        if (charCount.ContainsKey(c))
+            charCount[c]++;
+        else
+            charCount[c] = 1;
+    }
+    foreach (char c in s2)
+    {
+        if (charCount.ContainsKey(c))
+        {
+            charCount[c]--;
+            if (charCount[c] < 0)
+                return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+areAnagrams("listen", "silent");
+
+Console.WriteLine("Test");
---
+++ dsa/FindIndexofFirstOccurence/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int StrStr(string haystack, string needle)
+{
+    if (needle.Length == 0) return 0;
+    int h = haystack.Length;
+    int n = needle.Length;
+    int j = 0;
+    for (int i = 0; i < h; i++)
+    {
+        if (haystack[i] == needle[j])
+        {
+            j++;
+            if (j == n)
+            {
+                return i - n + 1;
+            }
+        }
+        else
+        {
+            i = i - j;
+            j = 0;
+        }
+    }
+    return -1;
+}
+
+StrStr("sadbutsad", "sad");
+
+Console.WriteLine("Test");
