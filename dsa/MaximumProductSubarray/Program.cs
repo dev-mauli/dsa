b448fbdc37b6934829b67c367a88bc1dd0142ca5
Pandurang Lad <premlad961@gmail.com>
2026-01-15 23:24:29 +0530

    Added MaximumProductSubarray and ValidParentheses

---
+++ dsa/MaximumProductSubarray/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int maxProduct(int[] arr)
+{
+    // code here
+    int n = arr.Length;
+    int maxSoFar = arr[0];
+    int maxEndingHere = arr[0];
+    int minEndingHere = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            (maxEndingHere, minEndingHere) = (minEndingHere, maxEndingHere);
+        }
+        maxEndingHere = Math.Max(arr[i], maxEndingHere * arr[i]);
+        minEndingHere = Math.Min(arr[i], minEndingHere * arr[i]);
+        maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+}
+
+maxProduct(new int[] { -2, 6, -3, -10, 0, 2 });
---
+++ dsa/ValidParentheses/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static bool IsValid(string s)
+{
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> parenthesesMap = new Dictionary<char, char>
+    {
+        { ')', '(' },
+        { '}', '{' },
+        { ']', '[' }
+    };
+    foreach (char c in s)
+    {
+        if (parenthesesMap.ContainsValue(c))
+        {
+            stack.Push(c);
+        }
+        else if (parenthesesMap.ContainsKey(c))
+        {
+            if (stack.Count == 0 || stack.Pop() != parenthesesMap[c])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+IsValid("()[]{}");
