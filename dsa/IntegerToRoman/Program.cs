c9869eafbcdf7210ca13c20a0eff12a43aac723f
Pandurang Lad <premlad961@gmail.com>
2026-01-16 17:04:41 +0530

    Added IntegerToRoman and Sort012s

---
+++ dsa/IntegerToRoman/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+static string IntToRoman(int num)
+{
+    Dictionary<int, string> valMap = new Dictionary<int, string>()
+    {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    };
+    StringBuilder roman = new StringBuilder();
+    for (int i = 0; i < num; i++)
+    {
+        foreach (var item in valMap)
+        {
+            while (num >= item.Key)
+            {
+                roman.Append(item.Value);
+                num -= item.Key;
+            }
+        }
+    }
+    return roman.ToString();
+}
+
+IntToRoman(1994);
---
+++ dsa/Sort012s/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void sort012(int[] arr)
+{
+    // code here
+    int n = arr.Length;
+    int low = 0;
+    int high = arr.Length - 1;
+    int mid = 0;
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            (arr[mid], arr[low]) = (arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            (arr[mid], arr[high]) = (arr[high], arr[mid]);
+            high--;
+        }
+    }
+}
+
+sort012(new int[] { 0, 1, 2, 0, 1, 2 });
