7e8972b7c5d60617349025d03752ab45a7243795
Pandurang Lad <premlad961@gmail.com>
2026-01-17 17:02:18 +0530

    Added NumberOfOccurrence and RomanToInteger

---
+++ dsa/NumberOfOccurrence/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static int countFreq(int[] arr, int target)
+{
+    // code here
+    int left = 0;
+    int right = arr.Length - 1;
+    int count = 0;
+    while (left <= right)
+    {
+        if (left == right)
+        {
+            if (arr[left] == target)
+                count++;
+        }
+        else
+        {
+            if (arr[left] == target)
+                count++;
+            if (arr[right] == target)
+                count++;
+        }
+        left++;
+        right--;
+    }
+    return count;
+}
+
+countFreq(new int[] { 1, 1, 2, 2, 2, 2, 3 }, 2);
---
+++ dsa/RomanToInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+static int RomanToInt(string s)
+{
+    Dictionary<char, int> valMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int total = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (i + 1 < s.Length && valMap[s[i]] < valMap[s[i + 1]])
+        {
+            total -= valMap[s[i]];
+        }
+        else
+        {
+            total += valMap[s[i]];
+        }
+    }
+    return total;
+}
+
+RomanToInt("MCMXCIV");
