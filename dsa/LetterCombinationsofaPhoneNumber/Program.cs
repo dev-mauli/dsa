4bc26eb6c84d9bee1481933b32dedf5c23ad6098
Pandurang Lad <premlad961@gmail.com>
2026-01-18 23:15:22 +0530

    Added LetterCombinationsofaPhoneNumber and SortedAndRotatedMinimum

---
+++ dsa/LetterCombinationsofaPhoneNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+static IList<string> LetterCombinations(string digits)
+{
+    Dictionary<char, string> phoneMap = new Dictionary<char, string>
+    {
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" },
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs" },
+        { '8', "tuv" },
+        { '9', "wxyz" }
+    };
+    List<string> result = new List<string>();
+    if (string.IsNullOrEmpty(digits))
+    {
+        return result;
+    }
+    void Backtrack(int index, StringBuilder path)
+    {
+        if (index == digits.Length)
+        {
+            result.Add(path.ToString());
+            return;
+        }
+        char digit = digits[index];
+        string letters = phoneMap[digit];
+        foreach (char letter in letters)
+        {
+            path.Append(letter);
+            Backtrack(index + 1, path);
+            path.Length--; // backtrack
+        }
+    }
+    if (!string.IsNullOrEmpty(digits))
+        Backtrack(0, new StringBuilder());
+    return result;
+}
+
+LetterCombinations("23");
---
+++ dsa/SortedAndRotatedMinimum/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int findMin(int[] nums)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[right])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
+
+findMin(new int[] { 3, 4, 5, 1, 2 });
