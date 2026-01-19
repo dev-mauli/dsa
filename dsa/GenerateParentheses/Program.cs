14b104f80dde72da903fb98017926106aee98840
Pandurang Lad <premlad961@gmail.com>
2026-01-19 12:20:19 +0530

    Added GenerateParentheses and SearchinRotatedSortedArray

---
+++ dsa/GenerateParentheses/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static IList<string> GenerateParenthesis(int n)
+{
+    int openParen = n;
+    int closeParen = n;
+    List<string> result = new List<string>();
+    void Backtrack(string current, int open, int close)
+    {
+        if (current.Length == n * 2)
+        {
+            result.Add(current);
+            return;
+        }
+        if (open > 0)
+        {
+            Backtrack(current + "(", open - 1, close);
+        }
+        if (close > open)
+        {
+            Backtrack(current + ")", open, close - 1);
+        }
+    }
+    Backtrack("", openParen, closeParen);
+    return result;
+}
+
+GenerateParenthesis(3);
---
+++ dsa/SearchinRotatedSortedArray/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static int search(int[] arr, int key)
+{
+    // Code Here
+    int left = 0, right = arr.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        // Left half is sorted
+        if (arr[left] <= arr[mid])
+        {
+            if (key >= arr[left] && key < arr[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        // Right half is sorted
+        else
+        {
+            if (key > arr[mid] && key <= arr[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0);
