53d3c66a1ccdd72ebc2434d6c5e1720712ace794
Pandurang Lad <premlad961@gmail.com>
2026-01-20 21:42:28 +0530

    Added FindHIndex

---
+++ dsa/FindHIndex/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int hIndex(int[] citations)
+{
+    Array.Sort(citations);
+    int n = citations.Length;
+
+    for (int i = 0; i < n; i++)
+    {
+        int papers = n - i;
+        if (citations[i] >= papers)
+        {
+            return papers;
+        }
+    }
+    return 0;
+}
+
+hIndex(new int[] { 3, 0, 5, 3, 0 });
