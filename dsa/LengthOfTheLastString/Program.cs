06586a7e782350f07931664493e29964646e91fa
Pandurang Lad <premlad961@gmail.com>
2026-01-21 08:51:41 +0530

    Added LengthOfTheLastString and PlusOne

---
+++ dsa/LengthOfTheLastString/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int LengthOfLastWord(string s)
+{
+    string oGs = s.TrimEnd();
+    string[] words = oGs.Split(' ');
+    int length = words[words.Length - 1].Length;
+    return length;
+}
+
+LengthOfLastWord("   fly me   to   the moon  ");
---
+++ dsa/PlusOne/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int[] PlusOne(int[] digits)
+{
+    for (int i = digits.Length - 1; i >= 0; i--)
+    {
+        if (digits[i] < 9)
+        {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+    int[] result = new int[digits.Length + 1];
+    result[0] = 1;
+    return result;
+}
+
+PlusOne(new int[] { 9 });
