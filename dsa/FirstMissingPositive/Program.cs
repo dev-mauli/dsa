64b7108c0e2babd5afe6020c4fae4accbe5c7655
Pandurang Lad <premlad961@gmail.com>
2026-01-28 20:03:54 +0530

    Added FirstMissingPositive and FirstlastPositionOfElementInArray

---
+++ dsa/FirstMissingPositive/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int missingNumber(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 0; i < n; i++)
+    {
+        while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i])
+        {
+            (arr[i], arr[arr[i] - 1]) = (arr[arr[i] - 1], arr[i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != i + 1)
+            return i + 1;
+    }
+    return n + 1;
+}
+
+missingNumber(new int[] { 2, -3, 4, 1, 1, 7 });
---
+++ dsa/FirstlastPositionOfElementInArray/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int[] SearchRange(int[] nums, int target)
+{
+    int[] result = new int[] { -1, -1 };
+
+    result[0] = FindFirst(nums, target);
+    if (result[0] == -1) return result; // target not found
+
+    result[1] = FindLast(nums, target);
+    return result;
+}
+
+static int FindFirst(int[] nums, int target)
+{
+    int left = 0, right = nums.Length - 1;
+    int index = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            index = mid;
+            right = mid - 1; // move left
+        }
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return index;
+}
+
+static int FindLast(int[] nums, int target)
+{
+    int left = 0, right = nums.Length - 1;
+    int index = -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            index = mid;
+            left = mid + 1; // move right
+        }
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return index;
+}
+
+
+SearchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 8);
