7308442f568ce25cdf593c8909f1edcc276363fc
Pandurang Lad <premlad961@gmail.com>
2026-02-21 22:01:42 +0530

    Added AddTwoNumbers and SumPairClosestToTarget

---
+++ dsa/AddTwoNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Hello, World!");
+
+static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+{
+    int carry = 0;
+    ListNode result = new ListNode(0);
+    ListNode ptr = result;
+
+    while (l1 != null || l2 != null)
+    {
+        int sum = carry + 0;
+        if (l1 != null)
+        {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null)
+        {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        carry = sum / 10;
+        sum = sum % 10;
+
+        ptr.next = new ListNode(sum);
+        ptr = ptr.next;
+    }
+
+    if (carry == 1)
+    {
+        ptr.next = new ListNode(1);
+    }
+
+    return result.next;
+}
+
+AddTwoNumbers(new ListNode(2) { next = new ListNode(4) { next = new ListNode(3) } }, new ListNode(5) { next = new ListNode(6) { next = new ListNode(4) } });
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
---
+++ dsa/SumPairClosestToTarget/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Hello, World!");
+
+static List<int> sumClosest(int[] arr, int target)
+{
+    // code here
+    Array.Sort(arr);
+    int left = 0;
+    int right = arr.Length - 1;
+    int minDifference = int.MaxValue;
+    List<int> result = new List<int>();
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+        int diff = Math.Abs(sum - target);
+        if (diff < minDifference)
+        {
+            minDifference = diff;
+            result.Clear();
+            result.Add(arr[left]);
+            result.Add(arr[right]);
+        }
+
+        if (sum < target)
+        {
+            left++;
+        }
+        else if (sum > target)
+        {
+            right--;
+        }
+        else
+        {
+            return result;
+        }
+    }
+    return result;
+}
+
+sumClosest(new int[] { 5, 2, 7, 1, 4 }, 10); // [4, 5]
