c87f59c3ce21819d03e170cd41ff6a470f24d8ce
Pandurang Lad <premlad961@gmail.com>
2026-02-24 22:47:51 +0530

    Added CountTheNoOfPossibleTriangles.csproj and DivideTwoIntegers.csproj

---
+++ dsa/CountTheNoOfPossibleTriangles/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hello, World!");
+
+static int countTriangles(int[] arr)
+{
+    // code here
+    int count = 0;
+    Array.Sort(arr);
+
+    for (int i = 2; i < arr.Length; i++)
+    {
+        int left = 0;
+        int right = i - 1;
+
+        while (left < right)
+        {
+            if (arr[left] + arr[right] > arr[i])
+            {
+                count += right - left;
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+    }
+    return count;
+}
+
+countTriangles(new int[] { 4, 6, 3, 7 });
---
+++ dsa/DivideTwoIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Hello, World!");
+
+static int Divide(int dividend, int divisor)
+{
+    // Edge case: overflow
+    if (dividend == int.MinValue && divisor == -1)
+        return int.MaxValue;
+
+    // Determine sign
+    bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+    // Convert to long to prevent overflow
+    long a = Math.Abs((long)dividend);
+    long b = Math.Abs((long)divisor);
+
+    int result = 0;
+
+    // Bit manipulation (shift divisor)
+    for (int i = 31; i >= 0; i--)
+    {
+        if ((a >> i) >= b)
+        {
+            result += (1 << i);
+            a -= (b << i);
+        }
+    }
+
+    return isNegative ? -result : result;
+}
+
+Divide(10, 3);
