823ce84a219ffc55fb20fc49331d10c2d7d7cb5f
Pandurang Lad <premlad961@gmail.com>
2026-02-25 21:13:46 +0530

    Added ReverseNodesInKGroup.csproj and SubArrayWithGivenSum.csproj

---
+++ dsa/ReverseNodesInKGroup/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Linq;
+
+Console.WriteLine("Hello, World!");
+
+static ListNode ReverseKGroup(ListNode head, int k)
+{
+    if (head == null) return null;
+
+    // Check if there are at least k nodes
+    ListNode temp = head;
+    for (int i = 0; i < k; i++)
+    {
+        if (temp == null)
+            return head; // Not enough nodes → no reverse
+        temp = temp.next;
+    }
+
+    // Reverse k nodes
+    ListNode prev = null;
+    ListNode curr = head;
+    ListNode next = null;
+    int count = 0;
+
+    while (curr != null && count < k)
+    {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+
+    // Recursively process remaining list
+    if (next != null)
+    {
+        head.next = ReverseKGroup(next, k);
+    }
+
+    return prev;
+}
+
+ReverseKGroup(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ dsa/SubArrayWithGivenSum/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+static List<int> SubarraySum(int[] arr, int target)
+{
+    // Code Here
+    int left = 0;
+    int sum = 0;
+
+    for (int right = 0; right < arr.Length; right++)
+    {
+        sum += arr[right];
+
+        while (sum > target && left <= right)
+        {
+            sum -= arr[left];
+            left++;
+        }
+
+        if (sum == target)
+        {
+            return arr.Skip(left).Take(right - left + 1).ToList();
+        }
+    }
+
+    return new List<int>();
+}
+
+SubarraySum(new int[] { 1, 2, 3, 7, 5 }, 12);
